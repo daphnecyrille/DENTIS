c13463b754cf67dc839f0fc3d7c7c1b203331473
Daphne <dsbaui@up.edu.ph>
2026-03-07 08:55:52 +0800

    Project structure setup for DENTIS

---
+++ src/main/java/com/dentis/DENTIS/DentisApplication.java
@@ -0,0 +1,13 @@
+package com.dentis.DENTIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dentis/DENTIS/DentisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentis.DENTIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
